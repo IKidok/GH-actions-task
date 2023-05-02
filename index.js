5bb5614f4e09dbc47a39b11e9d48ac3222340937
IKidok <vanya-shmatko@yandex.ru>
2023-05-02 07:55:07 +0400

    add fetch and display amout of open issues/prs

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { Octokit } = require("octokit");
+const main = async (_,__, ...arg) => {
+  const [ORG_NAME, REPO_NAME, AUTH_TOKEN]  = arg;
+  try {
+    const octokit = new Octokit({
+      auth: AUTH_TOKEN
+    })
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    const totalIssues = [];
+    let result = { length: 100 };
+    while (result.length === 100) {
+      result = (await octokit.request(`GET /repos/{org}/{repo}/issues?per_page=100&page=${ (totalIssues.length / 100) + 1 }&state=open`, {
+        org: ORG_NAME,
+        repo: REPO_NAME,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })).data;
+      console.log(result.length)
+      totalIssues.push(...result);
+    }
+    console.log(totalIssues.length)
+    const { issues, prs } = totalIssues.reduce((acc, issue) => {
+      [issueType, _] = issue.node_id.split('_');
+      if (issueType === 'PR') acc.issues += 1;
+      else acc.prs += 1;
+      return acc;
+    }, { issues: 0, prs: 0 })
+
+    core.setOutput('The amount of open PRs', prs);
+    core.setOutput('The amount of open Issues', issues);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main(...process.argv);
